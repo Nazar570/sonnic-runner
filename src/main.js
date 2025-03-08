cdf4ce9f27f74c9c1d55c57576018d1b4477989c
Nazar Suslyk <nazarsysluk@gmail.com>
2025-03-08 14:32:05 +0100

    Creating Main Menu

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import k from "./kaplayCtx";
+import mainMenu from "./scenes/mainMenu";
 
 k.loadSprite("chemical-bg", "graphics/chemical-bg.png");
 k.loadSprite("platforms", "graphics/platforms.png");
@@ -32,3 +33,7 @@ k.loadSound("hyper-ring", "sounds/HyperRing.wav");
 k.loadSound("jump", "sounds/Jump.wav");
 k.loadSound("ring", "sounds/Ring.wav");
 k.loadSound("city", "sounds/city.mp3");
+
+k.scene("main-menu", mainMenu);
+
+k.go("main-menu");
